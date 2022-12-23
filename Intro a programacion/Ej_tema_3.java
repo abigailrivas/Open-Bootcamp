f9c5bac4bea082cbb7fc5897d2a03f9148106928
Abi Rivas <lourdes.2502@gmail.com>
2022-12-23 19:00:16 -0300

    redo exercise

--- Intro a programacion/Ej_tema_3.java
+++ Intro a programacion/Ej_tema_3.java
@@ -3,23 +3,21 @@ public class Main {
     Coche miCoche = new Coche();
     miCoche.agregarPuertas(1);
 
-    System.out.println(sumar3(10,12,3));
+    sumar3(10,12,3);
   }
 
-  class Coche {
-    int puertas = 4;
-  
-    public void agregarPuertas(int a){
-      puertas =+ a;
-      System.out.println(puertas);
-    }
-  }  
-
-  int sumar3(int a, int b, int c){
-    return a + b + c;
+  public static void  sumar3(int a, int b, int c){
+    System.out.println(a + b + c);
   }
-
 }
 
+class Coche {
+  public int puertas = 4;
+
+  public void agregarPuertas(int a){
+    puertas =+ a;
+    System.out.println(puertas);
+  }
+}  
 
 
